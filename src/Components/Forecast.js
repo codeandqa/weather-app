3981ba72496064eb257b6c1e24803175e4408aa2
Aditya Kumar <aditya.kumar@salesforce.com>
2018-09-13 19:17:08 -0400

    added metrics

--- src/Components/Forecast.js
+++ src/Components/Forecast.js
@@ -1,29 +1,29 @@
 import React from 'react';
 import ZipCodeInput from './ZipCodeInput';
-import ResponsiveContainer from 'recharts/lib/component/ResponsiveContainer';
-import LineChart from 'recharts/lib/chart/LineChart';
-import Line from 'recharts/lib/cartesian/Line';
-import XAxis from 'recharts/lib/cartesian/XAxis';
-import YAxis from 'recharts/lib/cartesian/YAxis';
-import CartesianGrid from 'recharts/lib/cartesian/CartesianGrid';
-import Tooltip from 'recharts/lib/component/Tooltip';
-import Legend from 'recharts/lib/component/Legend';
+// import ResponsiveContainer from 'recharts/lib/component/ResponsiveContainer';
+// import LineChart from 'recharts/lib/chart/LineChart';
+// import Line from 'recharts/lib/cartesian/Line';
+// import XAxis from 'recharts/lib/cartesian/XAxis';
+// import YAxis from 'recharts/lib/cartesian/YAxis';
+// import CartesianGrid from 'recharts/lib/cartesian/CartesianGrid';
+// import Tooltip from 'recharts/lib/component/Tooltip';
+// import Legend from 'recharts/lib/component/Legend';
 import Typography from '@material-ui/core/Typography';
+import SimpleLineChart from './SimpleLineChart';
 const API_KEY='134d59bca896ab163d77fc4e06cbd20e';
 
 
 class Forecast extends React.Component {
   state = {
-    data: []
+    forecast: []
   };
-
+  
   getWeather = async (e) => {
     e.preventDefault();
     const zipCode = e.target.zipCode.value;
     if (zipCode) {
-      const api_call = await fetch(`https://api.openweathermap.org/data/2.5/forecast/?zip=${zipCode}&appid=${API_KEY}`);
+      const api_call = await fetch(`https://api.openweathermap.org/data/2.5/forecast/?zip=${zipCode}&appid=${API_KEY}&units=imperial`);
       const data = await api_call.json();
-      console.log(data);
       var arr = [];
       var len = data.list.length;
       for (var i = 0; i < len; i++) {
@@ -33,13 +33,10 @@ class Forecast extends React.Component {
               Temperature: data.list[i].main.temp
           });
       }
-
       this.setState({
-        data: arr
+        forecast: arr
       });
     } 
-    
-
   };
 
     render() {
@@ -49,18 +46,10 @@ class Forecast extends React.Component {
                 5 day / 3 hour forecast
               </Typography>
           <ZipCodeInput getWeather={this.getWeather} />
-          <p></p>
-           <ResponsiveContainer width="99%" height={320}>
-          <LineChart data={this.state.data}>
-            <XAxis dataKey="name" />
-            <YAxis />
-            <CartesianGrid vertical={false} strokeDasharray="3 3" />
-            <Tooltip />
-            <Legend />
-            <Line type="monotone" dataKey="Humidity" stroke="#82ca9d" />
-            <Line type="monotone" dataKey="Temperature" stroke="#8884d8" activeDot={{ r: 8 }} />
-          </LineChart>
-        </ResponsiveContainer>
+          
+          { 
+            this.state.forecast   &&  <SimpleLineChart forecastData={this.state.forecast} /> 
+          }
         </div>
       )
     };
--- src/Components/SimpleLineChart.js
+++ src/Components/SimpleLineChart.js
@@ -7,26 +7,28 @@ import YAxis from 'recharts/lib/cartesian/YAxis';
 import CartesianGrid from 'recharts/lib/cartesian/CartesianGrid';
 import Tooltip from 'recharts/lib/component/Tooltip';
 import Legend from 'recharts/lib/component/Legend';
-const API_KEY='134d59bca896ab163d77fc4e06cbd20e';
+// const API_KEY='134d59bca896ab163d77fc4e06cbd20e';
 
 
-class SimpleLineChart extends React.Component { 
-  render() {
+const SimpleLineChart = props => {
     return (
-        // 99% per https://github.com/recharts/recharts/issues/172
-        <ResponsiveContainer width="99%" height={320}>
-          <LineChart data={data}>
-            <XAxis dataKey="name" />
-            <YAxis />
-            <CartesianGrid vertical={false} strokeDasharray="3 3" />
-            <Tooltip />
-            <Legend />
-            <Line type="monotone" dataKey="Visits" stroke="#82ca9d" />
-            <Line type="monotone" dataKey="Orders" stroke="#8884d8" activeDot={{ r: 8 }} />
-          </LineChart>
-        </ResponsiveContainer>
+        <div>
+          { props.forecastData.length > 0 &&
+        
+            <ResponsiveContainer width="99%" height={320}>
+            <LineChart data={props.forecastData}>
+              <XAxis dataKey="name" />
+              <YAxis />
+              <CartesianGrid vertical={false} strokeDasharray="3 3" />
+              <Tooltip />
+              <Legend />
+              <Line type="monotone" dataKey="Humidity" stroke="#82ca9d" />
+              <Line type="monotone" dataKey="Temperature" stroke="#8884d8" activeDot={{ r: 8 }} />
+            </LineChart>
+          </ResponsiveContainer>
+          }
+      </div>
       );
-  };
-}
+};
 
 export default SimpleLineChart;
