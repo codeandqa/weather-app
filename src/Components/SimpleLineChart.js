dec7f42a2ffeadc31e3699996935eca7b0016432
Aditya Kumar <aditya.kumar@salesforce.com>
2018-09-12 23:37:05 -0400

    added forecast with chart

--- src/Components/SimpleLineChart.js
+++ src/Components/SimpleLineChart.js
@@ -9,15 +9,6 @@ import Tooltip from 'recharts/lib/component/Tooltip';
 import Legend from 'recharts/lib/component/Legend';
 const API_KEY='134d59bca896ab163d77fc4e06cbd20e';
 
-const data = [
-  { name: 'Mon', Visits: 2200, Orders: 3400 },
-  { name: 'Tue', Visits: 1280, Orders: 2398 },
-  { name: 'Wed', Visits: 5000, Orders: 4300 },
-  { name: 'Thu', Visits: 4780, Orders: 2908 },
-  { name: 'Fri', Visits: 5890, Orders: 4800 },
-  { name: 'Sat', Visits: 4390, Orders: 3800 },
-  { name: 'Sun', Visits: 4490, Orders: 4300 },
-];
 
 class SimpleLineChart extends React.Component { 
   render() {
