0384902436b3e37298eb968988ac124113e8f9f2
Aditya <learn@learnseleniumTesting.com>
2018-09-17 20:03:52 -0400

    added NavLink

--- src/Components/listItems.js
+++ src/Components/listItems.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListSubheader from '@material-ui/core/ListSubheader';
-import DashboardIcon from '@material-ui/icons/Dashboard';
-import BarChartIcon from '@material-ui/icons/BarChart';
-import AssignmentIcon from '@material-ui/icons/Assignment';
-
+import React from "react";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListSubheader from "@material-ui/core/ListSubheader";
+import DashboardIcon from "@material-ui/icons/Dashboard";
+import BarChartIcon from "@material-ui/icons/BarChart";
+import AssignmentIcon from "@material-ui/icons/Assignment";
+import { NavLink } from "react-router-dom";
 export const mainListItems = (
   <div>
-    <ListItem button component="a" href="/citycounty">
-      <ListItemIcon>
-        <DashboardIcon />
-      </ListItemIcon>
-      <ListItemText primary="City & Country" />
-    </ListItem>
-    <ListItem button component="a" href="/Forecast">
-      <ListItemIcon>
-        <BarChartIcon />
-      </ListItemIcon>
-      <ListItemText primary="Forecast" />
-    </ListItem>
+    <NavLink to="/citycounty">
+      <ListItem button component="a">
+        <ListItemIcon>
+          <DashboardIcon />
+        </ListItemIcon>
+        <ListItemText primary="City & Country" />
+      </ListItem>
+    </NavLink>
+
+    <NavLink to="/Forecast">
+      <ListItem button component="a">
+        <ListItemIcon>
+          <BarChartIcon />
+        </ListItemIcon>
+        <ListItemText primary="Forecast" />
+      </ListItem>
+    </NavLink>
   </div>
 );
 
